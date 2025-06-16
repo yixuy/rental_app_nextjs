8c7da3b59d6975a4a57568d168472feaf6dd5989
henryye <yixuy@sfu.ca>
2025-06-16 00:25:27 -0400

    Make remove bookmark

--- rental/components/BookmarkButton.jsx
+++ rental/components/BookmarkButton.jsx
@@ -7,8 +7,40 @@ import { toast } from "react-toastify";
 const BookmarkButton = ({ property }) => {
   const { data: session } = useSession();
   const userId = session?.user?.id;
-
   const [isBookmarked, setIsBookmarked] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (!userId) {
+      setLoading(false);
+      return;
+    }
+    const fetchBookmarkStatus = async () => {
+      try {
+        const res = await fetch("/api/bookmarks/check", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            propertyId: property._id,
+          }),
+        });
+
+        if (res.status === 200) {
+          const data = await res.json();
+          setIsBookmarked(data.isBookmarked);
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchBookmarkStatus();
+  }, [property._id, userId]);
+
   const handleClick = async () => {
     if (!userId) {
       toast.error("You need to sign in to bookmark a property");
@@ -35,15 +67,23 @@ const BookmarkButton = ({ property }) => {
       toast.error("Something went wrong");
     }
   };
-  return (
+  if (loading) return <p className="text-center">Loading ...</p>;
+  return isBookmarked ? (
     <div>
       <button
         onClick={handleClick}
-        className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+        className="bg-red-500 hover:bg-red-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
       >
-        <FaBookmark className="mr-2" /> Bookmark Property
+        <FaBookmark className="mr-2" /> Remove Bookmark
       </button>
     </div>
+  ) : (
+    <button
+      onClick={handleClick}
+      className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+    >
+      <FaBookmark className="mr-2" /> Bookmark Property
+    </button>
   );
 };
 
