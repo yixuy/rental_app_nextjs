18e90ea05081c5f52a230212f8e8eca7a9881e3a
henryye <yixuy@sfu.ca>
2025-06-16 01:47:04 -0400

    Finish Saved Page

--- rental/app/api/bookmarks/route.js
+++ rental/app/api/bookmarks/route.js
@@ -1,8 +1,33 @@
 import connectDB from "../../../config/database";
 import User from "@/models/User";
 import { getSessionUser } from "../../../utils/getSessionUser";
+import Property from "../../../models/Property";
 
 export const dynamic = "force-dynamic";
+
+// GET bookmarks for saved page
+
+export const GET = async () => {
+  try {
+    await connectDB();
+    const sessionUser = await getSessionUser();
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response("User ID is required ", { status: 401 });
+    }
+
+    const { userId } = sessionUser;
+
+    const user = await User.findOne({ _id: userId });
+
+    // get user bookmarks
+    const bookmarks = await Property.find({ _id: { $in: user.bookmarks } });
+
+    return new Response(JSON.stringify(bookmarks), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new Response("Something went wrong", { status: 500 });
+  }
+};
 
 export const POST = async (request) => {
   try {
